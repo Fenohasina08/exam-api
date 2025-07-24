1de69f5f18f82c786aa40116d3d27d80502d27a9
fenohasina <fenohasinaherimamy@gmail.com>
2025-07-24 09:46:27 +0000

    envoie

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List, Optional
 from starlette.responses import JSONResponse
-from typing import Optional
 
 app = FastAPI()
 
 
+students_db = []
+
+
+class Student(BaseModel):
+    Reference: str
+    FirstName: str
+    LastName: str
+    Age: int
+
 
 @app.get("/hello")
 async def say_hello_world():
-    return JSONResponse(content={"message": "Hello World"}, status_code=200)
+    return JSONResponse(content={"message": "Hello world"}, status_code=200)
 
 @app.get("/welcome")
 async def say_welcome(name: str):
     return JSONResponse(content={"message": f"Welcome {name}"}, status_code=200)
+
+
+ 
+
+@app.get("/students")
+async def get_students():
+    return JSONResponse(content=[student.dict() for student in students_db], status_code=200)
 
