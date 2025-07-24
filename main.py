f20823bf790967d45933c2e5aa41bc808e5b739b
fenohasina <fenohasinaherimamy@gmail.com>
2025-07-24 09:29:26 +0000

    envoi de Q1 et Q2

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
+from starlette.responses import JSONResponse
+from typing import Optional
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
+@app.get("/hello")
+async def say_hello_world():
+    return JSONResponse(content={"message": "Hello World"}, status_code=200)
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/welcome")
+async def say_welcome(name: str):
+    return JSONResponse(content={"message": f"Welcome {name}"}, status_code=200)
+
